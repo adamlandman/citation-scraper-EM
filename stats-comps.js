d730f69eba8ddf5292398c90c3c8b7d4ac115742
adamlandman <adamlguard-internet@yahoo.com>
2024-11-21 23:29:01 -0500

    Add files via upload
    Added comparands

---
+++ stats-comps.js
@@ -0,0 +1,229 @@
+var date = "Thu Nov 21 2024 23:26:37 GMT-0500 (Eastern Standard Time)"
+var data = [
+  {
+    "name": "Adam Landman",
+    "url": "https://scholar.google.com/citations?user=DTpQQUEAAAAJ&hl=en",
+    "affiliation": "Mass General Brigham and Harvard Medical School",
+    "keywords": [
+      "Clinical informatics",
+      "Artificial Intelligence",
+      "Digital Health",
+      "Emergency Medicine"
+    ],
+    "stats": {
+      "citations": [
+        "4902",
+        "3721"
+      ],
+      "hindex": [
+        "39",
+        "32"
+      ],
+      "i10index": [
+        "82",
+        "70"
+      ]
+    },
+    "year": "2005"
+  },
+  {
+    "name": "Allen Hsiao",
+    "url": "https://scholar.google.com/citations?user=7I3tj00AAAAJ&hl=en&oi=ao",
+    "affiliation": "Professor of Pediatrics & of Emergency Medicine, Yale School of Medicine",
+    "keywords": [
+      "Electronic health records",
+      "Telemedicine",
+      "Direct patient engagement in clinical research",
+      "Clinical decision support"
+    ],
+    "stats": {
+      "citations": [
+        "2298",
+        "1098"
+      ],
+      "hindex": [
+        "24",
+        "19"
+      ],
+      "i10index": [
+        "37",
+        "32"
+      ]
+    },
+    "year": "2005"
+  },
+  {
+    "name": "Jeffrey Ferranti",
+    "url": "https://scholar.google.com/citations?user=Dy_QGKsAAAAJ&hl=en&oi=ao",
+    "affiliation": "Professor of Pediatrics",
+    "keywords": [
+      "Medical Informatics"
+    ],
+    "stats": {
+      "citations": [
+        "3232",
+        "2196"
+      ],
+      "hindex": [
+        "19",
+        "13"
+      ],
+      "i10index": [
+        "28",
+        "16"
+      ]
+    },
+    "year": "2007"
+  },
+  {
+    "name": "John D. McGreevey III",
+    "url": "https://scholar.google.com/citations?user=xJDdsBEAAAAJ&hl=en&oi=ao",
+    "affiliation": "Associate Professor of Clinical Medicine, University of Pennsylvania",
+    "keywords": [
+      "Clinical informatics"
+    ],
+    "stats": {
+      "citations": [
+        "618",
+        "504"
+      ],
+      "hindex": [
+        "10",
+        "9"
+      ],
+      "i10index": [
+        "10",
+        "9"
+      ]
+    },
+    "year": "2001"
+  },
+  {
+    "name": "Feliciano 'Pele' B. Yu, Jr.",
+    "url": "https://scholar.google.com/citations?user=Fr2qssYAAAAJ&hl=en&oi=ao",
+    "affiliation": "Professor of Pediatrics and Biomedical Informatics, University of Arkansas for Medical Sciences",
+    "keywords": [
+      "Medical Informatics",
+      "Quality Improvement"
+    ],
+    "stats": {
+      "citations": [
+        "884",
+        "377"
+      ],
+      "hindex": [
+        "14",
+        "9"
+      ],
+      "i10index": [
+        "15",
+        "8"
+      ]
+    },
+    "year": "2007"
+  },
+  {
+    "name": "Paul Fu, Jr.",
+    "url": "https://scholar.google.com/citations?user=XPFi_FgAAAAJ&hl=en&oi=ao",
+    "affiliation": "Clinical Professor of Pediatrics and Professor of Health Policy and Management, UCLA",
+    "keywords": [
+      "Public Health Informatics",
+      "Medical Informatics",
+      "Quality Measurement",
+      "Quality of Care",
+      "Population Health"
+    ],
+    "stats": {
+      "citations": [
+        "2858",
+        "2212"
+      ],
+      "hindex": [
+        "11",
+        "7"
+      ],
+      "i10index": [
+        "12",
+        "7"
+      ]
+    },
+    "year": "2001"
+  },
+  {
+    "name": "Jonathan Nebeker",
+    "url": "https://scholar.google.com/citations?user=bHgQoQQAAAAJ&hl=en&oi=ao",
+    "affiliation": "Professor of Medicine, University of Utah",
+    "keywords": [
+      "Clinical Informatics",
+      "Electronic Health Records",
+      "Adverse Drug Events"
+    ],
+    "stats": {
+      "citations": [
+        "5566",
+        "1433"
+      ],
+      "hindex": [
+        "33",
+        "19"
+      ],
+      "i10index": [
+        "58",
+        "39"
+      ]
+    },
+    "year": "2003"
+  },
+  {
+    "name": "Karl Kochendorfer",
+    "url": "https://scholar.google.com/citations?user=mV4auZgAAAAJ&hl=en&oi=ao",
+    "affiliation": "Chief Health Information Officer; University of Illinois Chicago",
+    "keywords": [
+      "Informatics",
+      "Artificial Intelligence",
+      "Clinical Decision Support"
+    ],
+    "stats": {
+      "citations": [
+        "1340",
+        "381"
+      ],
+      "hindex": [
+        "12",
+        "9"
+      ],
+      "i10index": [
+        "15",
+        "9"
+      ]
+    },
+    "year": "2007"
+  },
+  {
+    "name": "John Vozenilek",
+    "url": "https://scholar.google.com/citations?user=7RlmMGEAAAAJ&hl=en&oi=ao",
+    "affiliation": "University of Illinois",
+    "keywords": [
+      "Simulation",
+      "BioDesign",
+      "Clinical Innovation",
+      "Instructional design",
+      "patient safety emergency medicine"
+    ],
+    "stats": {
+      "citations": [
+        "3877",
+        "1679"
+      ],
+      "hindex": [
+        "30",
+        "19"
+      ],
+      "i10index": [
+        "46",
+        "31"
+      ]
+    },
+    "year": "2005"
+  }
+];
